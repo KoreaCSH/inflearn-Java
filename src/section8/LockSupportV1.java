173bbe455b67355b7ba16bdc397c491ac21d9af7
csh <tmdgjs7495@naver.com>
2025-01-16 23:46:55 +0900

    [ch8] LockSupport 예제

---
+++ src/section8/LockSupportV1.java
@@ -0,0 +1,49 @@
+package section8;
+
+import java.util.concurrent.locks.LockSupport;
+
+import static util.MyLogger.*;
+import static util.ThreadUtils.sleep;
+
+public class LockSupportV1 {
+
+    public static void main(String[] args) {
+
+        Thread thread1 = new Thread(new ParkTest(), "Thread-1");
+        thread1.start();
+
+        sleep(100);
+        log("Thread-1 state: " + thread1.getState());
+
+        // 1. unpark test
+        /**
+        log("main -> unpark(Thread-1)");
+        LockSupport.unpark(thread1);
+        */
+
+        // park() 를 호출하여 스스로 대기상태에 빠질 수 있지만,
+        // 대기 상태의 스레드는 자신의 코드를 실행할 수 없기에 unpark() 에는 매개변수 존재한다.
+
+        // 2. interrupt test
+        // interrupt() 로도 WAITING 상태의 스레드를 깨울 수 있다.
+        /**
+        log("main -> interrupt(Thread-1)");
+        thread1.interrupt();
+         */
+    }
+
+    static class ParkTest implements Runnable {
+
+        @Override
+        public void run() {
+            log("park 시작");
+
+            // park() 시 RUNNABLE -> WAITING
+            LockSupport.park();
+
+            log("park 종료, status : " + Thread.currentThread().getState());
+            log("인터럽트 상태 : " + Thread.currentThread().isInterrupted());
+        }
+    }
+
+}
---
+++ src/section8/LockSupportV2.java
@@ -0,0 +1,33 @@
+package section8;
+
+import java.util.concurrent.locks.LockSupport;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class LockSupportV2 {
+
+    public static void main(String[] args) {
+
+        Thread thread1 = new Thread(new ParkTest(), "Thread-1");
+        thread1.start();
+
+        sleep(100);
+        log("Thread-1 state: " + thread1.getState());
+    }
+
+    static class ParkTest implements Runnable {
+
+        @Override
+        public void run() {
+            log("park 시작");
+
+            // parkNanos() 시 RUNNABLE -> TIMED_WAITING
+            LockSupport.parkNanos(2_000_000_000);
+            // Nano 초 이우 자동으로 unpark() 된다.
+            log("park 종료, status : " + Thread.currentThread().getState());
+            log("인터럽트 상태 : " + Thread.currentThread().isInterrupted());
+        }
+    }
+
+}
